4f1b62cad247de62290f01aff7542097c88d3bbc
JohnnyD3r <derventzis.cs@gmail.com>
2022-07-07 16:58:30 +0300

    Johnny D.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const body = document.querySelector('body'),
-    sidebar = body.querySelector('nav'),
-    toggle = body.querySelector(".toggle"),
-    modeSwitch = body.querySelector(".toggle-switch"),
-    modeText = body.querySelector(".mode-text");
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
 
 
 toggle.addEventListener("click" , () =>{
@@ -14,17 +14,17 @@ function toggleDark() {
   if (body.classList.contains('dark')) {
     body.classList.remove('dark');
     localStorage.setItem("theme", "light");
-    modeText.innerText = "Dark Mode";
+    modeText.innerText = "Light Mode";
   } else {
     body.classList.add('dark');
     localStorage.setItem("theme", "dark");
-    modeText.innerText = "Light Mode";
+    modeText.innerText = "Dark Mode";
   }
 }
 
 if (localStorage.getItem("theme") === "dark") {
   body.classList.add('dark');
-  modeText.innerText = "Light Mode";
+  modeText.innerText = "Dark Mode";
 }
 
 body.querySelector('.toggle-switch').addEventListener('click', toggleDark);
